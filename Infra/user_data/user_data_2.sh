815bcb6f595fcf10dd4116d27d53db75552b4c0a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-16 13:21:58 +0530

    Update user_data_2.sh

--- Infra/user_data/user_data_2.sh
+++ Infra/user_data/user_data_2.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 sudo yum update -y
-sudo amazon-linux-extras enable nginx
 sudo yum install -y nginx
-              
-sudo mkdir -p /usr/share/nginx/html/images
-
-echo '<html>
-<head><title>Images</title></head>
-<body>
-<h1>Image page </h1>
-</body>
-</html>' | sudo tee /usr/share/nginx/html/images/index.html > /dev/null
-
-# Configure Nginx for /images
-sudo tee /etc/nginx/conf.d/images.conf > /dev/null <<EOT
-server {
-  listen 80;
-  location /images {
-      root /usr/share/nginx/html;
-      index index.html;
-  }
-}
-EOT
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+echo "image" | sudo tee /usr/share/nginx/html/image.html
+sudo service nginx start

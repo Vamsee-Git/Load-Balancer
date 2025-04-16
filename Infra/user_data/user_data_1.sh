e770d747979fe2785f95e3e917924e55a33cd584
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-16 13:09:53 +0530

    Update user_data_1.sh

--- Infra/user_data/user_data_1.sh
+++ Infra/user_data/user_data_1.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 sudo yum update -y
-sudo amazon-linux-extras enable nginx
 sudo yum install -y nginx
-echo '<html>
-<head><title>Home</title></head>
-body>
-<h1>Home page</h1>
-</body>
-</html>' | sudo tee /usr/share/nginx/html/index.html > /dev/null
-
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+echo "Homepage Content" | sudo tee /usr/share/nginx/html/index.html
+sudo service nginx start

7a7de35a0bcc0d5381edb317ae5f1f9aacd2cc33
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-16 12:34:42 +0530

    Update user_data_3.sh

--- Infra/user_data/user_data_3.sh
+++ Infra/user_data/user_data_3.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 sudo yum update -y
+sudo amazon-linux-extras enable nginx1
 sudo yum install -y nginx
-echo "register" | sudo tee /usr/share/nginx/html/register.html
-sudo service nginx start
+              
+sudo mkdir -p /usr/share/nginx/html/register
+
+echo '<html>
+<head><title>Register</title></head>
+<body>
+<h1>Register page</h1>
+</body>
+</html>' | sudo tee /usr/share/nginx/html/register/index.html > /dev/null
+
+# Configure Nginx for /register
+sudo tee /etc/nginx/conf.d/register.conf > /dev/null <<EOT
+server {
+  listen 80;
+  location /register {
+    root /usr/share/nginx/html;
+    index index.html;
+  }
+}
+EOT

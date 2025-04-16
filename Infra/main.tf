01645d0d01427fa504dfe73e33999d5cedbf5dda
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-16 10:34:50 +0530

    Create main.tf

---
+++ Infra/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+module "vpc" {
+  source            = "./modules/vpc"
+  cidr_block        = "10.0.0.0/16"
+  availability_zones = ["us-west-1a", "us-west-1b", "us-west-1c"]
+}
+
+resource "aws_security_group" "instance" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+module "ec2_instance" {
+  source            = "./modules/ec2_instance"
+  ami               = "ami-0c55b159cbfafe1f0" # Example AMI ID, replace with your own
+  instance_type     = "t2.micro"
+  subnet_ids        = module.vpc.subnet_ids
+  security_group_id = aws_security_group.instance.id
+  user_data         = [
+    file("${path.module}/user_data/user_data_1.sh"),
+   }/user_data/user_data_2.sh"),
+    file("${path.module}/user_data/user_data_3.sh")
+  ]
+}
+
+module "target_group" {
+  source       = "./modules/target_group"
+  vpc_id       = module.vpc.vpc_id
+  instance_ids = module.ec2_instance.instance_ids
+}
+
+module "load_balancer" {
+  source            = "./modules/load_balancer"
+  security_group_id = aws_security_group.instance.id
+  subnet_ids        = module.vpc.subnet_ids
+  target_group_arns = module.target_group.target_group_arns
+}

02186133dc682210b6b09a707ee79e3d388d9cb4
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-16 10:52:19 +0530

    Create variables.tf

---
+++ Infra/modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}

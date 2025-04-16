c166521979278a3566d809638c1509e92d90a01f
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-16 11:21:51 +0530

    Update main.tf

--- Infra/modules/ec2_instance/main.tf
+++ Infra/modules/ec2_instance/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   ami                 = var.ami
   instance_type       = var.instance_type
   subnet_id           = element(var.subnet_ids, count.index)
-  security_group_ids  = [var.security_group_id]
+  vpc_security_group_ids = [var.security_group_id]
   associate_public_ip_address = true
   user_data           = element(var.user_data, count.index)
 
